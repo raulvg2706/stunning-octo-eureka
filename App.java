5ab0ba4e7d7f4ff6a48f783bdcfd619a18d103ea
raulvg <valbuenagarciaraul@gmail.com>
2024-01-22 09:59:10 +0100

    modificados nuevamente

--- App.java
+++ App.java
@@ -1,9 +1,11 @@
 public class App {
-  
-  public static void main(String[] args) {
-    System.out.println("Contenido 2a");
-    System.out.println("Contenido 3");
-    System.out.println("Contenido 2b");
-  }
-  
+    public static void main(String[] args) {
+        System.out.println("Raúl Valbuena García");
+        System.out.println("Contenido 2a");
+        System.out.println("Contenido 3");
+        System.out.println("Contenido 2b");
+        System.out.println("IES San Andres");
+	System.out.println("Villaobispo");
+    }
 }
+
